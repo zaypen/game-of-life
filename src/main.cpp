0e2c8eb0018439600de0a224a277650eb7d2af6b
Yapeng ZHANG <ypzhang@thoughtworks.com>
2018-06-07 17:29:50 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+
+int main() {
+/*    sf::Window window(sf::VideoMode(800, 600), "My window");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }*/
+    return 0;
+}
